3b19dace2c68d2651cf064abd048d9ab9a757673
IBALA <ishaqibrahimyusif@gmail.com>
2024-10-09 18:10:09 +0000

    Subscribe section done.

--- src/components/commonButton/index.jsx
+++ src/components/commonButton/index.jsx
@@ -7,14 +7,17 @@ function CommonButton({
   afterTextImg,
   imgClassName,
   imgAlt,
+  extraClasses,
+  type,
 }) {
   return (
     <button
       onClick={handleOnClick || null}
       className={
         className ||
-        "flex justify-center items-center gap-2 font-montserrat leading-none bg-coral-red px-7 py-4 rounded-full text-white"
+        `flex justify-center items-center gap-2 font-montserrat leading-none bg-coral-red px-7 py-4 rounded-full text-white ${extraClasses}`
       }
+      type={type || "button"}
     >
       {btnText || "Click Me"}
       {afterTextImg && (
--- src/pages/home/sections/subscribe.jsx
+++ src/pages/home/sections/subscribe.jsx
@@ -1,5 +1,35 @@
+import CommonButton from "../../../components/commonButton";
+
 function Subscribe() {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      id="contact-us"
+      className="flex justify-between items-center gap-10 max-container max-lg:flex-col"
+    >
+      <h3 className="font-palanquin font-bold text-4xl leading-[68px] lg:max-w-[40%]">
+        Sign Up for <span className="text-coral-red">Updates</span> & Newsletter
+      </h3>
+
+      <form className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="subscribe@nike.com"
+          className="input"
+        />
+
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <CommonButton
+            type={"submit"}
+            btnText={"Sign Up"}
+            extraClasses={"w-full"}
+            handleOnClick={(e) => e.preventDefault()}
+          />
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default Subscribe;
