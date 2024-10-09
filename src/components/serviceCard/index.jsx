18d04619b334f85769afd0c5019597c4def83bdf
IBALA <ishaqibrahimyusif@gmail.com>
2024-10-09 15:54:46 +0000

    Services section done.

---
+++ src/components/serviceCard/index.jsx
@@ -0,0 +1,26 @@
+import CommonIMG from "../commonImg";
+
+function ServiceCard({ imgURL, label, subtext }) {
+  return (
+    <div className="flex flex-col flex-1 max-w-96 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
+      <div className="w-11 h-11 flex justify-center items-center bg-coral-red rounded-full">
+        <CommonIMG
+          imgAlt={label}
+          imgWidth={24}
+          imgHeight={24}
+          imgSrc={imgURL}
+        />
+      </div>
+
+      <h3 className="mt-5 font-palanquin text-3xl leading-normal font-bold">
+        {label}
+      </h3>
+
+      <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">
+        {subtext}
+      </p>
+    </div>
+  );
+}
+
+export default ServiceCard;
--- src/pages/home/sections/services.jsx
+++ src/pages/home/sections/services.jsx
@@ -1,5 +1,15 @@
+import ServiceCard from "../../../components/serviceCard";
+import { services } from "../../../constants";
+
 function Services() {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services?.length > 0 &&
+        services.map((service) => (
+          <ServiceCard key={service.label} {...service} />
+        ))}
+    </section>
+  );
 }
 
 export default Services;
