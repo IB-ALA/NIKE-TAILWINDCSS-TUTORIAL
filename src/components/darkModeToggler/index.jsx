e220ee564f8b4911e431c60e0ebfb226c333fd4d
IBALA <ishaqibrahimyusif@gmail.com>
2025-04-17 00:24:24 +0000

    Used a better icon for darkmode toggler.

--- src/components/darkModeToggler/index.jsx
+++ src/components/darkModeToggler/index.jsx
@@ -1,24 +1,24 @@
 import { useContext } from "react";
-import { star } from "../../assets/icons";
-import CommonIMG from "../commonImg";
 import { GlobalContext } from "../../context";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+import CommonButton from "../commonButton";
 
 function DarkModeToggler() {
-  // get a better theme toggler
   const { darkMode, setDarkMode } = useContext(GlobalContext);
 
   return (
-    <button
-      onClick={() => setDarkMode(!darkMode)}
-      className="fixed bottom-8 right-8 z-50 cursor-pointer"
-    >
-      <CommonIMG
-        imgSrc={star}
-        imgHeight={50}
-        imgWidth={50}
-        imgClassName={"sm:w-[50px] sm:h-[50px] w-[30px] h-[30px]"}
-      />
-    </button>
+    <CommonButton
+      btnText={
+        darkMode ? (
+          <BsSunFill size={30} fill="#eab308" />
+        ) : (
+          <BsMoonFill size={30} />
+        )
+      }
+      handleOnClick={() => setDarkMode(!darkMode)}
+      className={"fixed bottom-8 right-8 z-50 cursor-pointer"}
+      btnTitle={darkMode ? "Switch to light theme" : "Switch to dark theme"}
+    />
   );
 }
 
