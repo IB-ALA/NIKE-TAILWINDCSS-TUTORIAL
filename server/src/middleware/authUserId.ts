5ee20d97e3cf2353c12f9bc602dbeef45a483065
IBALA <ishaqibrahimyusif@gmail.com>
2025-04-19 12:32:53 +0000

    __prev

--- server/src/middleware/authUserId.ts
+++
@@ -1,7 +0,0 @@
-import { users, UserType } from "../data/user";
-
-export function authUserID(id: string): UserType | undefined {
-  return users.find((user: UserType) => user.id === id);
-}
-
-// complete it later
---
+++ server/src/middleware/users.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { User } from "../data/user";
+import { getUser } from "../helpers/users";
+
+// we are extending Request 'cause we are attaching "user" to the requesst.
+export interface AuthenticatedRequest extends Request {
+  user?: User;
+}
+
+export function authUserID(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const userId: string = req.params.id;
+  console.log({ userId });
+  const user: User | undefined = getUser(userId);
+
+  if (!user) {
+    res.status(404).json({ error: "User varification failed" });
+    return;
+  }
+
+  // attach the user to the request object
+  req.user = user;
+
+  next();
+}
