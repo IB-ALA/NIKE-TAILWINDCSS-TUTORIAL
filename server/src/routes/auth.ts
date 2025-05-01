16dded88b448672d376a691fbf5fe9be384887e2
IBALA <ishaqibrahimyusif@gmail.com>
2025-05-01 17:37:17 +0000

    Backend: Done with the login process.

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import User from "../models/User";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import validator from "validator";
 
 const router = express.Router();
 
@@ -22,6 +24,11 @@ router.post("/register", async (req, res) => {
     return;
   }
 
+  if (!validator.isEmail(email)) {
+    res.status(404).json({ error: "Provide valid a email." });
+    return;
+  }
+
   try {
     // Check if email is already in use
     const existing = await User.findOne({ email });
@@ -39,11 +46,55 @@ router.post("/register", async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
       expiresIn: "1h",
     });
-    // console.log({ token });
 
     res.status(201).json({ message: "User registered successfully", token });
   } catch (err) {
     res.status(500).json({ message: "Error registering user" });
+  }
+});
+
+// Login Route
+router.post("/login", async (req, res) => {
+  if (!req.body) {
+    res.status(404).json({ error: "Provide request body!" });
+    return;
+  }
+
+  const { email, password }: { email: string; password: string } = req.body;
+
+  if (!email || !password) {
+    res.status(404).json({ error: "Provide all necessary info in body." });
+    return;
+  }
+
+  if (!validator.isEmail(email)) {
+    res.status(404).json({ error: "Provide valid a email." });
+    return;
+  }
+
+  try {
+    // Find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "Invalid credentials" });
+      return;
+    }
+
+    // Compare password with hashed one
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      res.status(400).json({ message: "Invalid credentials" });
+      return;
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
+      expiresIn: "1h",
+    });
+
+    res.json({ message: "Login successful", token });
+  } catch (err) {
+    res.status(500).json({ message: "Login failed" });
   }
 });
 
